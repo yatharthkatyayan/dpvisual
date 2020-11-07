d4ba8a8f352a4e0c3e9e0b70304148e5cb808048
yatharth katyayan <yatharth.18bec1121@abes.ac.in>
2020-11-07 13:14:47 +0530

    changes made on Sat, Nov  7, 2020  1:14:46 PM

--- src/Test2.jsx
+++ src/Test2.jsx
@@ -3,14 +3,37 @@ import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
 class Test2 extends Component {
   state = {};
+  openNav() {
+    // console.log("pressed");
+    document.getElementById("myNav").style.width = "100vw";
+  }
+
+  closeNav() {
+    //  console.log("closed");
+    let x = document.getElementById("myNav");
+    x.style.width = "0%";
+  }
   render() {
     return (
-      <div className="input_item">
-        <h1>test2</h1>
-        <div>
-          <Link to="/">
-            <button className="create_coin_button">HomePage</button>
-          </Link>
+      <div className="">
+        <div id="myNav" className="overlay fullnav">
+          <a
+            href="javascript:void(0)"
+            className="closebtn"
+            onClick={() => this.closeNav()}
+          >
+            &times;
+          </a>
+          <div className="overlay-content">
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Clients</a>
+            <a href="#">Contact</a>
+          </div>
+        </div>
+
+        <div className="fullnav" onClick={() => this.openNav()}>
+          &#9776; open
         </div>
       </div>
     );
