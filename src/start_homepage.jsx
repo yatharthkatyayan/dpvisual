efb266f40a49610d844831870ebf8f4b2c2f5c88
yatharth katyayan <yatharth.18bec1121@abes.ac.in>
2020-11-08 14:55:05 +0530

    changes made on Sun, Nov  8, 2020  2:55:05 PM

--- src/start_homepage.jsx
+++ src/start_homepage.jsx
@@ -3,9 +3,98 @@ class homepage extends Component {
   state = {};
   render() {
     return (
-      <div className="hope">
-        <svg viewBox={`0 0 1000 500`} className="svg">
-          <g>
+      <div className="homepage_screen">
+        <svg viewBox={`0 0 1000 500`} className="">
+          <g className="home_back_animation homepage_text">
+            <text x={100} y={100}>
+              D
+            </text>
+            <text x={200} y={200}>
+              D
+            </text>
+            <text x={300} y={300}>
+              D
+            </text>
+            <text x={400} y={400}>
+              D
+            </text>
+            <text x={500} y={50}>
+              D
+            </text>
+            <text x={600} y={150}>
+              D
+            </text>
+            <text x={700} y={250}>
+              D
+            </text>
+            <text x={800} y={350}>
+              D
+            </text>
+            <text x={900} y={450}>
+              D
+            </text>
+            <text x={150} y={35}>
+              D
+            </text>
+            <text x={250} y={135}>
+              D
+            </text>
+            <text x={350} y={235}>
+              D
+            </text>
+            <text x={450} y={335}>
+              D
+            </text>
+            <text x={550} y={435}>
+              D
+            </text>
+            <text x={650} y={75}>
+              D
+            </text>
+            <text x={750} y={175}>
+              D
+            </text>
+            <text x={850} y={275}>
+              D
+            </text>
+            <text x={950} y={375}>
+              D
+            </text>
+            <text x={25} y={475}>
+              D
+            </text>
+            <text x={45} y={250}>
+              D
+            </text>
+            <text x={250} y={150}>
+              D
+            </text>
+            <text x={350} y={350}>
+              D
+            </text>
+            <text x={450} y={150}>
+              D
+            </text>
+            <text x={550} y={350}>
+              D
+            </text>
+            <text x={650} y={250}>
+              D
+            </text>
+            <text x={750} y={150}>
+              D
+            </text>
+            <text x={850} y={350}>
+              D
+            </text>
+            <text x={950} y={150}>
+              D
+            </text>
+            <text x={500} y={350}>
+              D
+            </text>
+          </g>
+          <g className="homepage_text">
             <text x={0} y={50}>
               D
             </text>
@@ -28,7 +117,7 @@ class homepage extends Component {
               C
             </text>
           </g>
-          <g>
+          <g className="homepage_text">
             <text x={15} y={280}>
               P
             </text>
