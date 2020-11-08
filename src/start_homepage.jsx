d479382914c0b31503e1e98fb10bc4ecfbaa779e
yatharth katyayan <yatharth.18bec1121@abes.ac.in>
2020-11-08 13:37:39 +0530

    changes made on Sun, Nov  8, 2020  1:37:39 PM

--- src/start_homepage.jsx
+++ src/start_homepage.jsx
@@ -2,7 +2,33 @@ import React, { Component } from "react";
 class homepage extends Component {
   state = {};
   render() {
-    return <h1>homepage</h1>;
+    return (
+      <div className="hope">
+        <svg viewBox={`0 0 1000 500`} className="svg">
+          <text x={0} y={50}>
+            D
+          </text>
+          <text x={200} y={150}>
+            Y
+          </text>
+          <text x={350} y={90}>
+            N
+          </text>
+          <text x={500} y={180}>
+            A
+          </text>
+          <text x={630} y={30}>
+            M
+          </text>
+          <text x={780} y={140}>
+            I
+          </text>
+          <text x={900} y={110}>
+            C
+          </text>
+        </svg>
+      </div>
+    );
   }
 }
 
