104864a0b4b35d9f3f693f22ebdd0601d38e0bb9
yatharth katyayan <yatharth.18bec1121@abes.ac.in>
2020-11-10 08:34:09 +0530

    changes made on Tue, Nov 10, 2020  8:34:09 AM

--- src/problems/LIS/lis.jsx
+++ src/problems/LIS/lis.jsx
@@ -1,10 +1,36 @@
 import React, { Component } from "react";
 class LIS extends Component {
   state = {};
+  takeValues() {
+    let val = document.getElementById("Input_1").value;
+    let number_pattern = /\d+/g;
+    let numbers = val.match(number_pattern);
+    if (numbers) {
+      for (let i = 0; i < numbers.length; i++) {
+        numbers[i] = parseInt(numbers[i], 10);
+      }
+    }
+  }
   render() {
     return (
       <div className="parent_div">
-        <div className="menu"></div>
+        <div className="menu">
+          <div>
+            <input
+              id="Input_1"
+              className="input_lcs font_input"
+              required={true}
+              type="text"
+              placeholder="Input Array here"
+            />
+          </div>
+          <div>
+            <button className="lcs-visual" onClick={() => this.takeValues()}>
+              Visualize
+            </button>
+          </div>
+        </div>
+
         <div className="hope">
           <div className="padding_style"></div>
         </div>
