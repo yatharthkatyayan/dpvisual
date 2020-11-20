0c7df61144c9f46af9f3180984f1367b029e1862
yatharth katyayan <yatharth.18bec1121@abes.ac.in>
2020-11-20 20:03:49 +0530

    changes made on Fri, Nov 20, 2020  8:03:49 PM

--- src/dp_info.jsx
+++ src/dp_info.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 class DpInfo extends Component {
   state = {};
   render() {
-    return <h1>info about dp</h1>;
+    return <h1>please wait I am working on it</h1>;
   }
 }
 
