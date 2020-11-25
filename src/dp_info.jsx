1f7ef17b8a63c5fa6f09f46e052cbfe6abd96357
yatharth katyayan <yatharth.18bec1121@abes.ac.in>
2020-11-25 12:34:33 +0530

    changes made on Wed, Nov 25, 2020 12:34:33 PM

--- src/dp_info.jsx
+++ src/dp_info.jsx
@@ -2,7 +2,24 @@ import React, { Component } from "react";
 class DpInfo extends Component {
   state = {};
   render() {
-    return <h1>please wait I am working on it</h1>;
+    return (
+      <div className="info_div info">
+        Dynamic Programming is mainly an optimization over plain recursion.
+        <br />
+        <br></br>
+        Wherever we see a recursive solution that has repeated calls for same
+        inputs, we can optimize it using Dynamic Programming. The idea is to
+        simply store the results of subproblems, so that we do not have to
+        re-compute them when needed later.
+        <br />
+        <br />
+        This simple optimization reduces time complexities from exponential to
+        polynomial. For example, if we write simple recursive solution for
+        Fibonacci Numbers, we get exponential time complexity and if we optimize
+        it by storing solutions of subproblems, time complexity reduces to
+        linear.
+      </div>
+    );
   }
 }
 
--- src/start_homepage.jsx
+++ src/start_homepage.jsx
@@ -24,7 +24,8 @@ class homepage extends Component {
     for (let i = 0; i < gg.length; i++) {
       time1 = setTimeout(() => {
         greet.push(gg[i]);
-        document.getElementById("1").scrollIntoView();
+        if (document.getElementById("1"))
+          document.getElementById("1").scrollIntoView();
         this.setState({ starter: greet });
       }, time * i);
       timeout_array.push(time1);
@@ -41,7 +42,8 @@ class homepage extends Component {
     for (let i = 0; i < mid1.length; i++) {
       time1 = setTimeout(() => {
         middle1.push(mid1[i]);
-        document.getElementById("2").scrollIntoView();
+        if (document.getElementById("2"))
+          document.getElementById("2").scrollIntoView();
         this.setState({ midder1: middle1 });
       }, time * i);
       timeout_array.push(time1);
@@ -53,7 +55,8 @@ class homepage extends Component {
       for (let i = 0; i < mid2.length; i++) {
         let time2 = setTimeout(() => {
           middle2.push(mid2[i]);
-          document.getElementById("3").scrollIntoView();
+          if (document.getElementById("3"))
+            document.getElementById("3").scrollIntoView();
           this.setState({ midder2: middle2 });
         }, time * i);
         timeout_array.push(time2);
@@ -69,7 +72,8 @@ class homepage extends Component {
       for (let i = 0; i < mid3.length; i++) {
         let time2 = setTimeout(() => {
           middle3.push(mid3[i]);
-          document.getElementById("4").scrollIntoView();
+          if (document.getElementById("4"))
+            document.getElementById("4").scrollIntoView();
           this.setState({ midder3: middle3 });
         }, time * i);
         timeout_array.push(time2);
@@ -78,14 +82,15 @@ class homepage extends Component {
 
     timeout_array.push(time1);
     let here = (
-      <Link to="/dpvisual/info" style={{ color: "#2196f3" }}>
+      <Link key="unused_key" to="/dpvisual/info" style={{ color: "#2196f3" }}>
         here
       </Link>
     );
 
     time1 = setTimeout(() => {
       middle3.push(here);
-      document.getElementById("4").scrollIntoView();
+      if (document.getElementById("4"))
+        document.getElementById("4").scrollIntoView();
       this.setState({ midder3: middle3 });
     }, time * (mid1.length + mid2.length + mid3.length) + 100);
     timeout_array.push(time1);
@@ -102,7 +107,8 @@ class homepage extends Component {
     for (let i = 0; i < last_1.length; i++) {
       time1 = setTimeout(() => {
         last1.push(last_1[i]);
-        document.getElementById("5").scrollIntoView();
+        if (document.getElementById("5"))
+          document.getElementById("5").scrollIntoView();
         this.setState({ laster1: last1 });
       }, time * i);
       timeout_array.push(time1);
@@ -114,7 +120,8 @@ class homepage extends Component {
       for (let i = 0; i < last_2.length; i++) {
         setTimeout(() => {
           last2.push(last_2[i]);
-          document.getElementById("6").scrollIntoView();
+          if (document.getElementById("6"))
+            document.getElementById("6").scrollIntoView();
           this.setState({ laster2: last2 });
         }, time * i);
         timeout_array.push(time1);
@@ -127,7 +134,8 @@ class homepage extends Component {
       for (let i = 0; i < last_3.length; i++) {
         time1 = setTimeout(() => {
           last3.push(last_3[i]);
-          document.getElementById("7").scrollIntoView();
+          if (document.getElementById("7"))
+            document.getElementById("7").scrollIntoView();
           this.setState({ laster3: last3 });
         }, time * i);
         timeout_array.push(time1);
@@ -140,7 +148,8 @@ class homepage extends Component {
       for (let i = 0; i < last_4.length; i++) {
         time1 = setTimeout(() => {
           last4.push(last_4[i]);
-          document.getElementById("8").scrollIntoView();
+          if (document.getElementById("8"))
+            document.getElementById("8").scrollIntoView();
           this.setState({ laster4: last4 });
         }, time * i);
         timeout_array.push(time1);
@@ -153,7 +162,8 @@ class homepage extends Component {
       for (let i = 0; i < bye.length; i++) {
         time1 = setTimeout(() => {
           last5.push(bye[i]);
-          document.getElementById("9").scrollIntoView();
+          if (document.getElementById("9"))
+            document.getElementById("9").scrollIntoView();
           this.setState({ laster5: last5 });
         }, time * i);
         timeout_array.push(time1);
@@ -189,6 +199,9 @@ class homepage extends Component {
     setTimeout(() => {
       this.starter();
     }, 3000);
+  }
+  componentWillUnmount() {
+    this.clear();
   }
 
   render() {
